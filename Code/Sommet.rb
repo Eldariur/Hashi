4302076e6ef1bc8134fc41e5a849b860fb4807bd
Riguidel Hugo <s171369@ic2s110-03.univ-lemans.fr>
2020-02-13 18:09:43 +0100

    getCase

--- Code/Sommet.rb
+++ Code/Sommet.rb
@@ -1,10 +1,14 @@
 class Sommet
     #@listArete
-    attr_accessor :position
+    attr_accessor :position, :valeur
     def initialize(valeur, position)
         @valeur = valeur
         @position = position #la case dans lequel est le sommet
         @listArete = Array.new()
         @position.ajouterContenu(self)
+    end
+
+    def getCase()
+      return @position
     end
 end
