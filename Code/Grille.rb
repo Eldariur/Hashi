06f2043f3bafbb8c34f9ab8b04bf45a1d2dd7360
Riguidel Hugo <s171369@ic2s110-03.univ-lemans.fr>
2020-02-13 18:17:44 +0100

    ajout getCase() to_s() modification addSommet()

--- Code/Grille.rb
+++ Code/Grille.rb
@@ -1,29 +1,57 @@
 require "matrix"
 
 class Grille
-    #@longeur
+    #@longueur
     #@largeur
     #@table
     #@sommets
     attr_accessor :table
-    def initialize(longeur, largeur)
-        @longeur = longeur
+    def initialize(longueur, largeur)
+        @longueur = longueur
         @largeur = largeur
-        @table = Matrix.build(@longeur, @largeur){|row, col| Case.new(row, col)}
-        @sommets = []
-        @aretes = []
+        @table = Matrix.build(@longueur, @largeur){|row, col| Case.new(row, col)}
+        @sommets = Array.new()
+        @aretes = Array.new()
     end
 
     def completerInitialize()
-        for i in 0...@longeur do
+        for i in 0...@longueur do
             for j in 0...@largeur do
                 @table[i, j].setGrille(self)
             end
         end
     end
 
+    def getCase(x, y)
+      return @table[x, y]
+    end
+
     def addSommet(sommet)
-        @sommet = @sommet + [sommet]
+        @sommets.push(sommet)
+    end
+
+    def to_s()
+      s = ""
+      ajout = false
+      0.upto(@longueur - 1) do |i|
+        0.upto(@largeur - 1) do |j|
+          @sommets.each do |x|
+            if(x.getCase().x == i && x.getCase().y == j)
+              s += x.valeur.to_s()
+              ajout = true
+            end
+          end
+          @aretes.each do |x|
+            
+          end
+          if(ajout == false)
+            s += "X"
+          end
+          ajout = false
+        end
+        s += "\n"
+      end
+      return s + "\n"
     end
 
 end
