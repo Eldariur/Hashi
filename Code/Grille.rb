50358e79f924a7fa5149bc070e84cc6054e7e2cf
Gaiall <38868450+Gaiall@users.noreply.github.com>
2020-02-18 14:22:42 +0100

    Update des createur (nouveau: creer)
    Corrige le probleme de multiple methode a appeler pour creer un objet.
    Ne necessite plus que l'appel de creer

--- Code/Grille.rb
+++ Code/Grille.rb
@@ -6,14 +6,17 @@ class Grille
     #@table
     #@sommets
     attr_accessor :table
+    private_class_method :new, :completerInitialize
     def initialize(longueur, largeur)
         @longueur = longueur
         @largeur = largeur
-        @table = Matrix.build(@longueur, @largeur){|row, col| Case.new(row, col)}
+        @table = Matrix.build(@longueur, @largeur){|row, col| Case.creer(row, col)}
         @sommets = Array.new()
         @aretes = Array.new()
     end
 
+    #Complete le initialize
+    #ajoute self comme grille des cases de la matrice
     def completerInitialize()
         for i in 0...@longueur do
             for j in 0...@largeur do
@@ -22,46 +25,45 @@ class Grille
         end
     end
 
-    def getCase(x, y)
-      return @table[x, y]
+    #Creer un objet Grille proprement
+    def creer(longueur, largeur)
+        objet = new(longueur, largeur)
+        objet.completerInitialize()
+        return objet
     end
 
+    #recreer la matrice de case, puis appelle le completer initiliaze pour finir le boulot
+    def vider()
+        @table = Matrix.build(@longueur, @largeur){|row, col| Case.creer(row, col)}
+        completerInitialize()
+    end
+
+    #renvoie la case en x, y
+    def getCase(x, y)
+        return @table[x, y]
+    end
+
+    #ajoute le sommet a la liste de sommet
     def addSommet(sommet)
         @sommets.push(sommet)
     end
 
+    #ajoute l'arrete a la liste d'arrete
     def addArete(arete)
         @aretes.push(arete)
     end
 
-    def to_s()
-      s = ""
-      ajout = false
-      0.upto(@longueur - 1) do |i|
-        0.upto(@largeur - 1) do |j|
-          @sommets.each do |x|
-            if(x.getCase().x == i && x.getCase().y == j)
-              s += x.valeur.to_s()
-              ajout = true
+
+    #affiche la grille, case par case
+    def afficheToi()
+        @table.each{|c|
+            if((c.y)+1 >= @largeur)
+                c.afficheToi
+                puts("")
+            else
+                c.afficheToi
             end
-          end
-          @aretes.each do |x|
-            #p x.getListeCase()
-            x.getListeCase().each do |y|
-              if(y.x == i && y.y == j)
-                s += "|"
-                ajout = true
-              end
-            end
-          end
-          if(ajout == false)
-            s += "X"
-          end
-          ajout = false
-        end
-        s += "\n"
-      end
-      return s + "\n"
+        }
     end
 
 end
--- Code/Sommet.rb
+++ Code/Sommet.rb
@@ -1,24 +1,37 @@
 class Sommet
     #@listArete
     attr_accessor :position
+    private_class_method :new, :completerInitialize
     def initialize(valeur, position)
         @valeur = valeur
         @position = position #la case dans lequel est le sommet
         @listeArete = Array.new()
     end
 
+    #ajoute self comme contenu de la case a laquelle il est et a la liste de sommet de la grille
     def completerInitialize()
         @position.ajouterContenu(self)
+        @position.grille.addSommet(self)
     end
 
+    #creer un Sommet proprement
+    def creer(valeur, position)
+        objet = new(valeur, position)
+        objet.completerInitialize()
+        return objet
+    end
+
+    #defini la valeur du sommet
     def setValeur(valeur)
         @valeur = valeur
     end
 
+    #ajoute une arrete a la liste de ses arrete
     def ajouterArete(arete)
         @listeArete << (arete)
     end
 
+    #retire une arrete de la liste de ses arrete
     def retirerArete(arete)
         @listeArete.delete(arete)
     end
