725a627b1497aff87dd2f001c09978ad17a7fc0b
Riguidel Hugo <s171369@ic2s110-03.univ-lemans.fr>
2020-02-18 14:01:12 +0100

    Mise à jour de la doc

--- Code/rDoc.rb
+++ Code/rDoc.rb
@@ -1,67 +1,78 @@
-##Programme pour voir l'utilisation de rdoc, pour compiler la documentation, rdoc nom.rb, pour voir si il manque des commentaires, rdoc nom.rb -C
+## Programme pour voir l'utilisation de rdoc, pour compiler la documentation, rdoc nom.rb, pour voir si il manque des commentaires, rdoc nom.rb -C
 
 
 # Classe permettant de détailler l'utilisation de la doc
 class Doc
-	##Partie variables d'instance
-	#Variables d'instances de la classe Doc
-	#@nom1 -> Détails rapide de la variables
-	#@nom2 -> Pareil
-	#@nom3 -> Défini de base dans la classe
-	def Doc.creer(param1,param2)
-		new(param1,param2)
+
+
+
+	## Partie variables d'instance
+
+	# @nom1 -> Détails rapide de la variables
+	# @nom2 -> Pareil
+	# @nom3 -> Défini de base dans la classe
+
+	def Doc.creer(param1, param2)
+		new(param1, param2)
 	end
 
-	#Privatise le new
+	# Privatise le new
 	private_class_method :new
 
-	##Intialise
 
-	#Initialisation de la class Doc
-	#
-	#===Parametre
-	#
-	#* +param1+ : param1 petite définition(Son nom,une date ...)
-	#* +param2+ : param2 idem
 
-	def initialize(param1,param2)
+	## Partie initialize
+
+	# Initialisation de la class Doc
+	#
+	# === Parametre
+	#
+	# * +param1+ : param1 petite définition(Son nom,une date ...)
+	# * +param2+ : param2 idem
+	def initialize(param1, param2)
 		@nom1 = param1
 		@nom2 = param2
 		@nom3 = "TEST"
 	end
 
-	##Partie Autorisation
-	#Autorise les autres classes à lire
+
+
+	## Partie accesseurs
+
+	# Accesseur get sur l'attribut nom1
 	attr_reader :nom1
 
-	#Autorise les autres classes à ecrire
+	# Accesseur set sur l'attribut nom1
 	attr_writer :nom2
 
-	#Autorise les autres classes à lire et à écrire
+	# Accesseur get et set sur l'attribut nom1
 	attr_accessor :nom3
 
-	##Partie méthode
-	##Methode sans param et renvoie rien
-	#Détails juste ce que réalise la méthode
+
+
+	## Partie méthodes
+
+	## Methode sans param et renvoie rien
+	# Détails juste ce que réalise la méthode
 	def test1()
 		puts @nom1
 	end
 
-	##Methode avec param mais renvoie rien
-	#Détails juste ce que réalise la méthode
-	#===Parametre
-	#* +param+ : param avec une description
+	## Methode avec param mais renvoie rien
+	# Détails juste ce que réalise la méthode
+	# === Parametre
+	# * +param+ : param avec une description
 	def test2(param)
 		puts @nom2.to_s + " " + param
 	end
 
-	##Methode avec param plus renvoie
-	#Détails juste ce que réalise la méthode
-	#===Parametre
-	#* +param+ : param avec une description
+	## Methode avec param plus renvoie
+	# Détails juste ce que réalise la méthode
+	# === Parametre
+	# * +param+ : param avec une description
 	#
-	#===Return
-	#* +@nom3+ : param avec une description
+	# === Return
+	# * +@nom3+ : param avec une description
 	def test3(param)
 		puts @nom2 +param
 		return @nom3
@@ -72,7 +83,7 @@ class Doc
 		print "coucou"
 	end
 
-	##Methode non commenter, pour tester rDoc -C
+	## Methode non commenter, pour tester rDoc -C
 
 end
 
