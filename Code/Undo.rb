6b626a63a66fef7632f3512335803222467801d9
Snalts <alexandre.jodeau@orange.fr>
2020-03-16 15:55:22 +0100

    Ajout class undo

---
+++ Code/Undo.rb
@@ -0,0 +1,43 @@
+require_relative 'Arete.rb'
+
+#Class undo, permet de stocker les actions du joueurs
+class Undo
+
+	# @tabAction -> Stocke les arretes qu'il a jouer
+	
+	def Undo.creer()
+		new()
+	end
+
+	#Privatise le new
+	private_class_method :new
+	
+	#Initialisation de la class Undo
+	#
+	# Instancie la variable @tabAction en array
+	def initialize()
+		@tabAction = Array.new
+	end
+	
+	# empile la dernière Arete ajouter dans la grile
+	#
+	# ===Paramètres
+	# * +arete+ : Ajout de la dernière arete jouer par le joueur
+	def empile(Arete arete)
+		@tabAction.push(arete)
+	end
+
+	# empile la dernière Arete ajouter dans la grile
+	#
+	# ===Paramètres
+	# * +arete+ : depile la dernière arete jouer par le joueur
+	def depile()
+		return @tabAction.pop()
+	end
+
+	@Override
+	def to_s()
+		return "Array Undo : " +  @tabAction.to_s()
+	end
+
+end
