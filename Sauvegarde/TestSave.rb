35166297546df04052542f86e0338724f5a1d089
Gaiall <nathan.manson.72@gmail.com>
2020-03-20 09:24:22 +0100

    Résolution du bug de boucle infinie

--- Sauvegarde/TestSave.rb
+++ Sauvegarde/TestSave.rb
@@ -36,6 +36,9 @@ arete11 = Arete.creer(sommet10, sommet11)
 
 #Affichage des grilles
 grille.afficher()
+#
+=begin
+Sauvegarde.genenerDossier();
 
 while(gets == nil) do end
 
@@ -61,7 +64,7 @@ puts "Validation hypothese"
 Sauvegarde.validerHypothese()
 grille.afficher()
 
-=begin
+
 c = Chrono.nouveau()
 
 threads = []
@@ -94,7 +97,6 @@ puts saveres
 saveres = Sauvegarde.nouvelle(nil,nil,3,true).charger()
 puts saveres
 
-
-
-Sauvegarde.deleteAllSave()
 =end
+
+#Sauvegarde.deleteAllSave()
