12428f2b117924b2bb8bb8db3f0d2b7db4762801
Snalts <alexandre.jodeau@orange.fr>
2020-04-06 16:09:31 +0200

    Ajout des boutons retour dans fenetre menu et menujouer

--- Interface/FenetreMenuJouer.rb
+++ Interface/FenetreMenuJouer.rb
@@ -1,30 +1,35 @@
-class FenetreMenuJouer < Gtk::Box
+class	 FenetreMenuJouer < Gtk::Box
 
-  def initialize(window)
+  def initialize(window,widgAnte)
     super(Gtk::Orientation::VERTICAL)
     @@fenetre = window
-    #@@fenPre = self
+    @@fenPre = widgAnte
     tbl = Gtk::Table.new(1,1)
     vBox = Gtk::Box.new(Gtk::Orientation::VERTICAL)
     bouton1 = UnBoutonPerso.new("Tutoriel", "BoutonMenu")
     bouton2 = UnBoutonPerso.new("Normal", "BoutonMenu")
     bouton3 = UnBoutonPerso.new("Contre-la-montre", "BoutonMenu")
-
+	boutonRetour = UnBoutonPerso.new("<--","BoutonMenu")
     bouton1.signal_connect('clicked') {
-      @@fenetre.changerWidget(FenetreTuto.new(@@fenetre))
+      @@fenetre.changerWidget(FenetreTuto.new(@@fenetre,self))
     }
 
     bouton2.signal_connect('clicked') {
-      @@fenetre.changerWidget(FenetreModesDifficultes.new(@@fenetre))
+      @@fenetre.changerWidget(FenetreModesDifficultes.new(@@fenetre,self))
     }
 
     bouton3.signal_connect('clicked') {
-      @@fenetre.changerWidget(FenetreModeChrono.new(@@fenetre))
+      @@fenetre.changerWidget(FenetreModeChrono.new(@@fenetre,self))
     }
+
+	boutonRetour.signal_connect('clicked'){
+		@@fenetre.changerWidget(@@fenPre);
+	}
 
     vBox.add(bouton1)
     vBox.add(bouton2)
     vBox.add(bouton3)
+    vBox.add(boutonRetour)
     tbl.attach(vBox,0,1,0,1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::EXPAND, 0, @@fenetre.default_size[1] / 3)
     self.add(tbl)
 
