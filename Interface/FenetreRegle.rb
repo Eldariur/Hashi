dd5a74545c046dc4e5e4e2832493402f12fd3abe
Snalts <alexandre.jodeau@orange.fr>
2020-04-11 16:45:07 +0200

    Ajout documentation

--- Interface/FenetreRegle.rb
+++ Interface/FenetreRegle.rb
@@ -5,15 +5,15 @@ class FenetreRegle < Gtk::Box
     super(Gtk::Orientation::VERTICAL)
 
     lbl1 = UnLabelPerso.new("	Bienvenue dans le jeu Hashiwokakero (Le hashi pour être plus rapide, ou en français 'construire des ponts'), est un jeu dans laquelle votre réflexion sera votre seul ami.
-				Les règles sont simples, relier tous les sommets entre eux en créant des ponts, et oui il y à des sommets, mais ces sommets son plutôt spéciaux voyez-vous.
-				Ils sont numérotés ! , cette numérotation annonce le nombre de ponts pouvant être liée à cette île, exemple : un sommet à 1 peut avoir qu'un seul pont,
-				un sommet à 3 peut avoir 3 pont.
-				Il faudra alors les compléter, par exemple un sommet à 5 devra avoir 5 ponts partant de ce sommet.
-				Cependant, il n'existe que deux type de pont, simple et double, vous avez la base du hashi,
+				Les règles sont simples, relier tous les sommets entre eux en créant des ponts, et oui il y a des sommets, mais ces sommets sont plutôt spéciaux voyez-vous.
+				Ils sont numérotés ! , cette numérotation annonce le nombre de ponts pouvant être liée à cette île, exemple : un sommet à 1 ne peut avoir qu'un seul pont,
+				un sommet à 3 peut avoir 3 ponts.
+				Il faudra alors les compléter, par exemple un sommet à 5 devra avoir 5 ponts partants de ce sommet.
+				Cependant, il n'existe que deux types de pont, simple et double, vous avez la base du hashi,
 				je vous laisse découvrire ce magnifique monde.
 				Au cas ou je remets un petit résumé si vous voulez relire les règles rapidement :
 				-Tous les sommets doivent être reliés et complétés.
-				-Uniquement des ponts doubles et simple, (dans tous les cas vous ne pouvez que faire ça comme pont dans notre jeu donc bon)
+				-Uniquement des ponts doubles et simple.
 				-Chaque sommet à une numérotation, remplisser celui ci pour le valider. ","UnLabelBlanc")
     self.add(lbl1)
 	boutonRetour = UnBoutonPerso.new("Retour","btnQuit")
