e8becd81a5037217b3e3d40eb40fc8b9df196d3b
Shishi42 <benriv4272@gmail.com>
2020-04-12 15:16:34 +0200

    Changement pour main.rb mais en dehors ça ne marchera pas

--- Classement/ConnectSqlite3.rb
+++ Classement/ConnectSqlite3.rb
@@ -4,7 +4,7 @@ require "active_record"
 ActiveRecord::Base.establish_connection(
 
   :adapter => "sqlite3",
-  :database => "../Classement/data.sqlite",
+  :database => "./Classement/data.sqlite",
   :timeout => 3600
 
 )
